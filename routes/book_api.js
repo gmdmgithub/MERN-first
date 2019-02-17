f82ec5f06dfbddd4d6d94ae46c297f3efa5c03a1
gmdmgithub <gmika@interia.pl>
2019-02-18 00:24:50 +0100

    delete a book added - from parameter

--- routes/book_api.js
+++ routes/book_api.js
@@ -25,6 +25,10 @@ router.get('/:id', (req, res, next) => {
     console.log('Get the book');
     next();
 });
+
+// @route   POST api/books
+// @desc    Create a book
+// @access  Public
 router.post('/', (req, res, next) => {
 
     //some static Book - for test
@@ -55,16 +59,25 @@ router.post('/', (req, res, next) => {
     .catch(err => console.log(err))
     
 });
+
+
 router.delete('/:id', (req, res, next) => {
     console.log('delete a book');
 
-    Book.findOneAndDelete({
-            "_id": req.params.id
-        })
-        .then(data => res.json(data))
-        .catch(next)
+    Book.findOneAndDelete({ "_id": req.params.id})
+        .then(() => res.json({ success: true }))
+        .catch(err => res.status(404).json({ success: false }));
+
+    // alternative way - first find
+    Book.findById(req.params.id)
+    .then(book => book.remove().then(() => res.json({ success: true })))
+    .catch(err => {
+        console.log(err);
+        res.status(404).json({ success: false })
+    });
 
 })
+
 
 router.put('/:id', (req, res, next) => {
     console.log('update a book');
